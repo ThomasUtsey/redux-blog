d3d9cd666a4bcb1b53ff4d8741d84795f4886cb9
Thomas Utsey <tom@edgegluer.com>
2020-03-23 21:45:48 -0400

    Add framework for App Build

---
+++ src/Components/PostList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PostList extends React.Component{
+
+  componentDidMount = () =>{
+
+  }
+
+render(){
+return (
+  <div>PostList</div>
+)
+}
+
+}
+
+export default PostList
